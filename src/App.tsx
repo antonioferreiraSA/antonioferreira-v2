d4d99c21aa54da2a6bd99317f8f36678b9bfc7bd
antonioferreiraSA <ferreiratoni112@gmail.com>
2021-05-01 07:49:42 +0200

    learning about hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 
+import Booklist from "./components/books/Booklist";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -12,6 +13,7 @@ function App() {
         content="Hello world I am the HEADER"
       />
       <Footer content="This is the footer" />
+      <Booklist/>
     </>
   );
 }
---
+++ src/constants/api.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://t9jt3myad3.execute-api.eu-west-2.amazonaws.com/api/books"
